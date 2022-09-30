775d85911e137e612318a373c003ede3541f57a0
Ad Net <214075982@mycput.ac.za>
2022-09-30 11:21:42 +0200

    Library and Library Members Domain, Factory and Repository

---
+++ src/main/java/za/ac/cput/repository/Library_MemberRepository.java
@@ -0,0 +1,16 @@
+package za.ac.cput.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.Library_Member;
+
+import java.util.List;
+
+
+@Repository
+public interface Library_MemberRepository extends JpaRepository<Library_Member, String> {
+
+
+    //public List<Library_Librarian> findByID(int id);
+
+}
