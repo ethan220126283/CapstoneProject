26bc901c10bc005c15acafe3a6efa8351ce3360b
Ad Net <214075982@mycput.ac.za>
2022-10-09 19:38:10 +0200

    Services for Library_member & Member as well as tests

---
+++ src/main/java/za/ac/cput/library_management/service/Library_MemberService.java
@@ -0,0 +1,17 @@
+package za.ac.cput.library_management.service;
+
+/*   Name:       Adrian Bennett
+ *   Student#:   214075982
+ *   Date:       09/10/2022
+ */
+
+
+import za.ac.cput.library_management.domain.Library_Member;
+
+
+
+
+public interface Library_MemberService  extends IService<Library_Member,String>{
+
+
+}
---
+++ src/main/java/za/ac/cput/library_management/service/MemberService.java
@@ -0,0 +1,14 @@
+package za.ac.cput.library_management.service;
+
+/*   Name:       Adrian Bennett
+ *   Student#:   214075982
+ *   Date:       09/10/2022
+ */
+
+
+import za.ac.cput.library_management.domain.Member;
+
+
+public interface MemberService extends IService<Member, String>{
+
+}
