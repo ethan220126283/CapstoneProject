bd865b7f86011b319db02709d96bb58b9538eec7
Lana-Africa <216166640@mycput.ac.za>
2022-10-10 01:48:30 +0200

    Bookline Service Impl

---
+++ src/main/java/za/ac/cput/library_management/service/impl/BooklineServiceImpl.java
@@ -0,0 +1,40 @@
+package za.ac.cput.library_management.service.impl;
+/*
+    BooklineServiceImpl.java
+    Lana Africa (216166640)
+    Capstone Project - Service Implementation
+*/
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.library_management.domain.Bookline;
+import za.ac.cput.library_management.repository.BooklineRepository;
+import za.ac.cput.library_management.service.BooklineService;
+
+@Service
+public class BooklineServiceImpl implements BooklineService {
+
+    private final BooklineRepository repository;
+
+    @Autowired
+    BooklineServiceImpl(BooklineRepository repository) {
+        this.repository = repository;
+    }
+    @Override
+    public Bookline save(Bookline bookline) {
+        return this.repository.save(bookline);
+    }
+
+    @Override
+    public Bookline read(String id) {
+        return this.repository.findById(id).orElse(null);
+    }
+
+    @Override
+    public boolean delete(String id) {
+        if (this.repository.existsById(id)) {
+            this.repository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
