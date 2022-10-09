7fbc55659f76c6cbfc52d529c9cf58318d9f70d2
Lana-Africa <216166640@mycput.ac.za>
2022-10-10 01:44:52 +0200

    Bookline Service

---
+++ src/main/java/za/ac/cput/library_management/service/BooklineService.java
@@ -0,0 +1,10 @@
+package za.ac.cput.library_management.service;
+/*
+    BooklineService.java
+    Lana Africa (216166640)
+    Capstone Project - Service
+*/
+import za.ac.cput.library_management.domain.Bookline;
+
+public interface BooklineService extends IService<Bookline, String> {
+}
