b63f0dad1c0789d2995b1e4a147c3f539ee2304e
Faeedh <219174288@mycput.ac.za>
2022-10-10 13:41:17 +0200

    Library_Librarian and Librarian Service Layer

---
+++ src/main/java/za/ac/cput/library_management/service/LibrarianService.java
@@ -0,0 +1,11 @@
+package za.ac.cput.library_management.service;
+
+/*   Name:       Mogammad Faeedh Daniels
+ *   Student#:   219174288
+ *   LibrarianService.java
+ */
+
+import za.ac.cput.library_management.domain.Librarian;
+
+public interface LibrarianService extends IService<Librarian,String>{
+}
