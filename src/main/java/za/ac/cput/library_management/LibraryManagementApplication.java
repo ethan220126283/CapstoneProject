91855dff1e5c1572aa4a440f12c560bbd2927ce7
ethan220126283 <220126283@mycput.ac.za>
2022-10-19 13:10:08 +0200

    GUI implementation

--- src/main/java/za/ac/cput/library_management/LibraryManagementApplication.java
+++ src/main/java/za/ac/cput/library_management/LibraryManagementApplication.java
@@ -2,12 +2,23 @@ package za.ac.cput.library_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import za.ac.cput.library_management.ui.libraryGUI;
+
+import java.awt.*;
 
 @SpringBootApplication
 public class LibraryManagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LibraryManagementApplication.class, args);
+        var ctx = new SpringApplicationBuilder(libraryGUI.class)
+                .headless(false)
+                .run(args);
+
+        EventQueue.invokeLater(() -> {
+            var ex = ctx.getBean(libraryGUI.class);
+            ex.setVisible(true);
+        });
     }
 
 }
