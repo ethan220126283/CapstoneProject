dd0d87bfe5e50a743418868c40685b3f1dfe8c03
ethan220126283 <220126283@mycput.ac.za>
2022-10-20 17:26:44 +0200

    Object[][] array for member

--- src/main/java/za/ac/cput/library_management/api/MemberAPI.java
+++ src/main/java/za/ac/cput/library_management/api/MemberAPI.java
@@ -7,6 +7,7 @@ import za.ac.cput.library_management.domain.Member;
 import za.ac.cput.library_management.factory.MemberFactory;
 import za.ac.cput.library_management.service.impl.MemberServiceImpl;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -47,8 +48,8 @@ public class MemberAPI {
       return memberService.read(id);
     }
 
-    public void addMember(String id,String name,String address, String tell, String status){
-        Member member = MemberFactory.createMember(id,name,address,tell,status);
+    public void addMember(String id,String name,String address, String tel, String status){
+        Member member = MemberFactory.createMember(id,name,address,tel,status);
         memberService.save(member);
     }
 
@@ -62,13 +63,23 @@ public class MemberAPI {
     public Object[][] getMembersTable() {
 
         List<Member> members = getMembers();
-        Object[][] data;
+        Object[][] data = new Object[members.size()][];
 
-        /*for (int i = 0; i < members.size(); i++) {
-            data.
-        }*/
-
-        return null;
+        for (int i = 0; i < members.size(); i++) {
+            Member member = members.get(i);
+            int j = 0;
+            data[i][j] = member.getId();
+            j++;
+            data[i][j] = member.getName();
+            j++;
+            data[i][j] = member.getAddress();
+            j++;
+            data[i][j] = member.getTel();
+            j++;
+            data[i][j] = member.getStatus();
+        }
+        //System.out.println(Arrays.deepToString(data));
+        return data;
     }
 
 }
