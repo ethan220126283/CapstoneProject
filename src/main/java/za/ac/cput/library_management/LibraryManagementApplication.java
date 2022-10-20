229880efe1b111dc2b074994d4ca88bbed4ca995
Darryll Merkeur <darryll.merkeur@gmail.com>
2022-10-20 18:29:23 +0200

    Login GUI test

--- src/main/java/za/ac/cput/library_management/LibraryManagementApplication.java
+++ src/main/java/za/ac/cput/library_management/LibraryManagementApplication.java
@@ -3,6 +3,7 @@ package za.ac.cput.library_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import za.ac.cput.library_management.ui.LoginView;
 import za.ac.cput.library_management.ui.libraryGUI;
 
 import java.awt.*;
@@ -11,7 +12,7 @@ import java.awt.*;
 public class LibraryManagementApplication {
 
     public static void main(String[] args) {
-
+/*
         var ctx = new SpringApplicationBuilder(libraryGUI.class)
                 .headless(false)
                 .run(args);
@@ -19,6 +20,16 @@ public class LibraryManagementApplication {
         EventQueue.invokeLater(() -> {
             var ex = ctx.getBean(libraryGUI.class);
             ex.setVisible(true);
+        });*/
+
+
+        var ctx = new SpringApplicationBuilder(LoginView.class)
+                .headless(false)
+                .run(args);
+
+        EventQueue.invokeLater(() -> {
+            var ex = ctx.getBean(LoginView.class);
+            ex.showGUI();
         });
 
     }
