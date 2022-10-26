9df27cf96d48a28e9c45b668d1eb14cf47d23063
ethan220126283 <220126283@mycput.ac.za>
2022-10-26 23:56:22 +0200

    2nd Final Commit

--- src/main/java/za/ac/cput/library_management/ui/libraryGUI.java
+++ src/main/java/za/ac/cput/library_management/ui/libraryGUI.java
@@ -463,7 +463,17 @@ public class libraryGUI extends JFrame {
         booklineReturn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int result = JOptionPane.showConfirmDialog(pnlMain, "Are you sure you wish to delete this Bookline?");
+                if (result == JOptionPane.YES_OPTION) {
+                    int row = booklineTable.getSelectedRow();
+                    if (row == -1) {
+                        JOptionPane.showMessageDialog(pnlMain, "Error: No bookline is selected", "Error", JOptionPane.ERROR_MESSAGE);
+                    } else {
+                        String select = itemTable.getModel().getValueAt(row, 0).toString();
+                        booklineAPI.deleteById(select);
+                        createTables();
+                    }
+                }
             }
         });
 
